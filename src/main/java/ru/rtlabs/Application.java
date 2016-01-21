c3b61708f68afc078710351dc083257d12430226
DanStopka1985 <stepan.danilov@rtlabs.ru>
2016-01-21 18:06:36 +0300

    [nf] apache poi example added

--- src/main/java/ru/rtlabs/Application.java
+++ src/main/java/ru/rtlabs/Application.java
@@ -1,18 +1,22 @@
 package ru.rtlabs;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) throws IOException, SQLException {
+	public static void main(String[] args) throws IOException, SQLException, InvalidFormatException {
 
 		ApplicationContext app = SpringApplication.run(Application.class, args);
 //		JdbcTemplate jdbcTemplate = app.getBean(JdbcTemplate.class);
@@ -25,6 +29,10 @@ public class Application {
 //		A a = app.getBean(A.class);
 		Loader loader = app.getBean(Loader.class);
 		loader.createSrc("d:/src/Действующий_справочник_ЛС.csv");
+
+		loader.xlsTempWork();
+
+
 
 	}
 }
--- src/main/java/ru/rtlabs/Loader.java
+++ src/main/java/ru/rtlabs/Loader.java
@@ -3,6 +3,10 @@ package ru.rtlabs;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,11 +14,10 @@ import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -54,5 +57,55 @@ public class Loader {
 
             System.out.println(record.get("DrugID"));
         }
+    }
+
+    public void xlsTempWork() throws IOException, InvalidFormatException {
+//        FileInputStream file = new FileInputStream(new File("d:\\projects\\loaders\\loader_little_files\\src\\x.xlsx"));
+//
+//        //Get the workbook instance for XLS file
+//        HSSFWorkbook workbook = new HSSFWorkbook(file);
+//
+//        //Get first sheet from the workbook
+//        HSSFSheet sheet = workbook.getSheetAt(0);
+//
+//        //Get iterator to all the rows in current sheet
+//        Iterator<Row> rowIterator = sheet.iterator();
+//
+//
+//        while (rowIterator.hasNext()){
+//            Row row = rowIterator.next();
+//            Iterator<Cell> cellIterator = row.cellIterator();
+//            while (cellIterator.hasNext()){
+//                Cell cell = cellIterator.next();
+//                System.out.println(cell.getStringCellValue());
+//            }
+//        }
+
+
+        InputStream inp = new FileInputStream("d:\\projects\\loaders\\loader_little_files\\src\\фарм.гр.xls");
+        Workbook wb = WorkbookFactory.create(inp);
+
+        Sheet sheet = wb.getSheetAt(0);
+        Row row = sheet.getRow(1);
+        System.out.println("level: " + row.getOutlineLevel());
+        row = sheet.getRow(2);
+        System.out.println("level: " + row.getOutlineLevel());
+        row = sheet.getRow(3);
+        System.out.println("level: " + row.getOutlineLevel());
+        row = sheet.getRow(4);
+        System.out.println("level: " + row.getOutlineLevel());
+
+        Cell cell = row.getCell(3);
+        if (cell == null)
+            cell = row.createCell(3);
+        cell.setCellType(Cell.CELL_TYPE_STRING);
+        cell.setCellValue("a test");
+
+        // Write the output to a file
+        FileOutputStream fileOut = new FileOutputStream("d:\\projects\\loaders\\loader_little_files\\src\\фарм.гр.xls");
+        wb.write(fileOut);
+        fileOut.close();
+
+
     }
 }
