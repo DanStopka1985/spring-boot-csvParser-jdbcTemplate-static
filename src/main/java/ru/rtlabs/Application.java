6c72cbf1fbc41f7bcf4c0ceb60203cf3e89adf1d
DanStopka1985 <stepan.danilov@rtlabs.ru>
2016-01-25 11:16:23 +0300

    [nf] loader little files

--- src/main/java/ru/rtlabs/Application.java
+++ src/main/java/ru/rtlabs/Application.java
@@ -1,44 +1,25 @@
 package ru.rtlabs;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Workbook;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
+import ru.rtlabs.little_files_loader.Inn;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+
 import java.net.URISyntaxException;
 import java.sql.SQLException;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) throws IOException, SQLException, InvalidFormatException, URISyntaxException {
+	public static void main(String[] args) throws IOException, InvalidFormatException, SQLException {
 
 		ApplicationContext app = SpringApplication.run(Application.class, args);
-//		JdbcTemplate jdbcTemplate = app.getBean(JdbcTemplate.class);
-//		SqlRowSet rs = jdbcTemplate.queryForRowSet("select 10000 a");
-//		while (rs.next()){
-//			System.out.println(rs.getString("a"));
-//
-//		}
-//
-//		A a = app.getBean(A.class);
 		Loader loader = app.getBean(Loader.class);
-		//loader.createSrc("d:/src/Действующий_справочник_ЛС.csv");
-
-		loader.xlsTempWork();
-
-
-
-		System.out.println("Working Directory = " +
-				System.getProperty("user.dir"));
-
+		loader.execute();
 
 	}
 }
--- src/main/java/ru/rtlabs/Loader.java
+++ src/main/java/ru/rtlabs/Loader.java
@@ -1,25 +1,12 @@
 package ru.rtlabs;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.stereotype.Component;
+import ru.rtlabs.little_files_loader.Inn;
 
-import javax.sql.DataSource;
 import java.io.*;
-import java.net.URISyntaxException;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * Created by Stepan Danilov on 13.01.2016.
@@ -28,86 +15,83 @@ import java.util.List;
 @Component
 public class Loader {
 
-    @Autowired
-    JdbcTemplate jt;
-    @Autowired
-    DataSource ds;
-
-    public void createSrc(String path) throws IOException, SQLException {
-
-        jt.execute("drop table if exists loader_l1_src;");
-
-        Connection connection = ds.getConnection();
-
-        ScriptUtils.executeSqlScript(connection, new FileSystemResource("d:\\projects\\loaders\\l1\\tmp\\create_table.sql"));
-
-        CSVParser csvFileParser = null;
-        FileReader fileReader = null;
-
-
-        fileReader = new FileReader(path);
-
-        CSVFormat csvFileFormat = CSVFormat.newFormat(';').withQuote('"').withHeader();
-        csvFileParser = new CSVParser(fileReader, csvFileFormat);
-
-        List csvRecords = csvFileParser.getRecords();
-
-
-        for (int i = 1; i < csvRecords.size(); i++) {
-            CSVRecord record = (CSVRecord) csvRecords.get(i);
-
-            System.out.println(record.get("DrugID"));
-        }
-    }
-
-    public void xlsTempWork() throws IOException, InvalidFormatException, URISyntaxException {
-//        FileInputStream file = new FileInputStream(new File("d:\\projects\\loaders\\loader_little_files\\src\\x.xlsx"));
+//    @Autowired
+//    JdbcTemplate jt;
+//    @Autowired
+//    DataSource ds;
 //
-//        //Get the workbook instance for XLS file
-//        HSSFWorkbook workbook = new HSSFWorkbook(file);
+//    public void createSrc(String path) throws IOException, SQLException {
 //
-//        //Get first sheet from the workbook
-//        HSSFSheet sheet = workbook.getSheetAt(0);
+//        jt.execute("drop table if exists loader_l1_src;");
 //
-//        //Get iterator to all the rows in current sheet
-//        Iterator<Row> rowIterator = sheet.iterator();
+//        Connection connection = ds.getConnection();
+//
+//        ScriptUtils.executeSqlScript(connection, new FileSystemResource("d:\\projects\\loaders\\l1\\tmp\\create_table.sql"));
+//
+//        CSVParser csvFileParser = null;
+//        FileReader fileReader = null;
 //
 //
-//        while (rowIterator.hasNext()){
-//            Row row = rowIterator.next();
-//            Iterator<Cell> cellIterator = row.cellIterator();
-//            while (cellIterator.hasNext()){
-//                Cell cell = cellIterator.next();
-//                System.out.println(cell.getStringCellValue());
-//            }
+//        fileReader = new FileReader(path);
+//
+//        CSVFormat csvFileFormat = CSVFormat.newFormat(';').withQuote('"').withHeader();
+//        csvFileParser = new CSVParser(fileReader, csvFileFormat);
+//
+//        List csvRecords = csvFileParser.getRecords();
+//
+//
+//        for (int i = 1; i < csvRecords.size(); i++) {
+//            CSVRecord record = (CSVRecord) csvRecords.get(i);
+//
+//            System.out.println(record.get("DrugID"));
 //        }
+//    }
+//
+//    public void xlsTempWork() throws IOException, InvalidFormatException, URISyntaxException {
+//
+//        InputStream inp = new FileInputStream("d:\\projects\\loaders\\loader_little_files\\src\\атх.xls");
+//        Workbook wb = WorkbookFactory.create(inp);
+//
+////        Sheet sheet = wb.getSheetAt(0);
+////        Row row = sheet.getRow(1);
+////        System.out.println("level: " + row.getOutlineLevel());
+////        row = sheet.getRow(2);
+////        System.out.println("level: " + row.getOutlineLevel());
+////        row = sheet.getRow(3);
+////        System.out.println("level: " + row.getOutlineLevel());
+////        row = sheet.getRow(4);
+////        System.out.println("level: " + row.getOutlineLevel());
+//
+////        Cell cell = row.getCell(3);
+////        if (cell == null)
+////            cell = row.createCell(3);
+////        cell.setCellType(Cell.CELL_TYPE_STRING);
+////        cell.setCellValue("a test");
+//
+//        // Write the output to a file
+////        FileOutputStream fileOut = new FileOutputStream("d:\\projects\\loaders\\loader_little_files\\src\\1.xls");
+////        wb.write(fileOut);
+////        fileOut.close();
+//
+//        Sheet sheet = wb.getSheetAt(0);
+//        Row row = sheet.getRow(2);
+//
+//        Cell cell = row.getCell(0);
+//        System.out.println(cell.getStringCellValue());
+//
+//
+//
+//
+//    }
 
+    @Autowired
+    Inn inn;
 
-        InputStream inp = new FileInputStream("d:\\projects\\loaders\\loader_little_files\\src\\фарм.гр.xls");
-        Workbook wb = WorkbookFactory.create(inp);
-
-        Sheet sheet = wb.getSheetAt(0);
-        Row row = sheet.getRow(1);
-        System.out.println("level: " + row.getOutlineLevel());
-        row = sheet.getRow(2);
-        System.out.println("level: " + row.getOutlineLevel());
-        row = sheet.getRow(3);
-        System.out.println("level: " + row.getOutlineLevel());
-        row = sheet.getRow(4);
-        System.out.println("level: " + row.getOutlineLevel());
-
-        Cell cell = row.getCell(3);
-        if (cell == null)
-            cell = row.createCell(3);
-        cell.setCellType(Cell.CELL_TYPE_STRING);
-        cell.setCellValue("a test");
-
-        // Write the output to a file
-        FileOutputStream fileOut = new FileOutputStream("d:\\projects\\loaders\\loader_little_files\\src\\фарм.гр.xls");
-        wb.write(fileOut);
-        fileOut.close();
-
-
-
+    public void execute() throws InvalidFormatException, SQLException, IOException {
+        inn.create();
+        inn.loadSrc();
+        System.out.println("inn is valid:" + inn.isValid());
     }
+
+
 }
